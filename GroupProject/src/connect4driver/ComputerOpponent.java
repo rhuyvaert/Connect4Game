97197f57511c0babc8a2d472291bd88e4e0f265b
rhuyvaert <129347392+rhuyvaert@users.noreply.github.com>
2023-04-11 11:41:46 -0400

    added base of computer opponent

---
+++ GroupProject/src/connect4driver/ComputerOpponent.java
@@ -0,0 +1,14 @@
+package connect4driver;
+
+
+public class ComputerOpponent {
+    
+    //constructor
+    
+    //Function ranking all 7 columns from best to worst move
+    
+    //function the takes an input of diffuculty option 
+    //decides which of the 7 columns to place the piece in based on difficulty
+    
+    //function that sends that returns the move to be placed
+}
